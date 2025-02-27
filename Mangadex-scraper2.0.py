02f30dd7405e7c38c3769b1e5a59095a47ab98dc
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-02-27 15:38:55 +0100

    update Mangadex-scraper2.0.py

--- Mangadex-scraper2.0.py
+++ Mangadex-scraper2.0.py
@@ -18,11 +18,11 @@ class Mangadex:
         manga_json.raise_for_status
         manga_data=(manga_json.json())["data"]
         if manga_data==[]:
-		    print("This manga doesn't exist")
-		    sys.exit()
-	    self.manga_title=manga_data[0]["attributes"]["title"][lang]
+        	print("This manga doesn't exist")
+    		sys.exit()
+    	self.manga_title=manga_data[0]["attributes"]["title"][lang]
     	self.manga_description=manga_data[0]["attributes"]["description"][lang]
-	    manga_id=manga_data[0]["id"]
+        manga_id=manga_data[0]["id"]
     	return manga_id
 
     def __str__(self): 
@@ -32,3 +32,7 @@ class Mangadex:
     def get_chapters(self, manga_id): 
         url=f"https://api.mangadex.org/manga/{manga_id}/aggregate?translatedLanguage%5B%5D={self.lang}"
         chapters_json = requests.get(url)
+        chapters_json.raise_for_status
+        chapters=(chapter_json.json())["volumes"]["none"]["chapters"]
+    	chapter_ids={float(num) : chapter["id"] for num,chapter in chapters.items()}
+    	return chapter_ids
