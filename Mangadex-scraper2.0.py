7982fb6a2245ac1cc5654a1221a39ae9b38f02a7
Juwonthefirst <ajibolajuwon5@gmail.com>
2025-02-28 20:54:29 +0100

    update Mangadex-scraper2.0.py

--- Mangadex-scraper2.0.py
+++ Mangadex-scraper2.0.py
@@ -8,7 +8,7 @@ class Mangadex:
         self.lang = lang
         self.manga_title = None
         self.manga_description = None
-        
+        self.length = 0
         
     def search(self):
         name = self.name.split()
@@ -19,15 +19,15 @@ class Mangadex:
         manga_data=(manga_json.json())["data"]
         if manga_data==[]:
         	print("This manga doesn't exist")
-    		sys.exit()
+       		sys.exit()
     	self.manga_title=manga_data[0]["attributes"]["title"][lang]
     	self.manga_description=manga_data[0]["attributes"]["description"][lang]
         manga_id=manga_data[0]["id"]
+        self.length = 1
     	return manga_id
 
-    def __str__(self): 
+    def __str__ (self): 
         return f"{self.manga_title}\n {self.manga_description}"
-    
   
     def get_chapters(self, manga_id): 
         url=f"https://api.mangadex.org/manga/{manga_id}/aggregate?translatedLanguage%5B%5D={self.lang}"
@@ -35,4 +35,8 @@ class Mangadex:
         chapters_json.raise_for_status
         chapters=(chapter_json.json())["volumes"]["none"]["chapters"]
     	chapter_ids={float(num) : chapter["id"] for num,chapter in chapters.items()}
+    	self.length = len()
     	return chapter_ids
+    	
+    def __len__ (self): 
+        return self.length
